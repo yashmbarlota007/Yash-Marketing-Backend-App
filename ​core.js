d611cc6e8e03cadf0cf067850aaf85787f54fd87
yashmbarlota007 <yashmbarlota007@gmail.com>
2026-09-05 18:41:23 +0530

    Update ​core.js

--- ​core.js
+++ ​core.js
@@ -1,5 +1,4 @@
-<script>
-    const API_URL = "https://script.google.com/macros/s/AKfycbzQ7W09dn_1gyNSq_PCPrKLX6ApgB52Ob2_1BnmRe5SdYTMNyc8kRwudr80pe2QFCQZCg/exec"; 
+const API_URL = "https://script.google.com/macros/s/AKfycbzQ7W09dn_1gyNSq_PCPrKLX6ApgB52Ob2_1BnmRe5SdYTMNyc8kRwudr80pe2QFCQZCg/exec"; 
     
     // Trimmed to 9 Stages for Max Efficiency
     const STAGE_NAMES = [
@@ -1321,4 +1320,3 @@
             `;
         });
     }
-</script>
