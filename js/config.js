dc14354d0fcc7dae3ab30bfcdf4c00bdfa48553f
yashmbarlota007 <yashmbarlota007@gmail.com>
2026-09-05 21:07:28 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // js/config.js
 
-const API_URL = "https://script.google.com/macros/s/AKfycbzQ7W09dn_1gyNSq_PCPrKLX6ApgB52Ob2_1BnmRe5SdYTMNyc8kRwudr80pe2QFCQZCg/exec"; 
+const API_URL = "https://script.google.com/macros/s/AKfycbw3ye5kfsC4y6_qUa6MobtitHsTcC9r5l30YDeDd9nnznfUiUPq4sd7riGNQA8e8r5wwQ/exec"; 
 
 // Trimmed to 9 Stages for Max Efficiency
 const STAGE_NAMES = [
